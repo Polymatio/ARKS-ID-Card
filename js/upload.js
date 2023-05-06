d359ecffd60143d3093c0fa6e5b5e3de02fcc6e1
Jesus Porras <102770370+Polymatio@users.noreply.github.com>
2023-05-05 21:12:06 -0700

    added function to upload images on card route

--- js/upload.js
+++ js/upload.js
@@ -1,3 +1,11 @@
-const image_input = document.querySelector("#image_input");
+const characterPicture = document.querySelector("#characterPicture");
 let uploaded_image = "";
 
+characterPicture.addEventListener("change", function() {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+        uploaded_image = reader.result;
+        document.querySelector("#idPicture").style.backgroundImage = `url(${uploaded_image})`
+    });
+    reader.readAsDataURL(this.files[0]);
+})
